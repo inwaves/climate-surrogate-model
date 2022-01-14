cbc3117289595c433354f809650d879ce494994f
Andrei <inwaves@live.com>
2022-01-14 11:15:01 +0000

    Fix dataframe size issue

--- optimise.py
+++ optimise.py
@@ -12,9 +12,6 @@ warnings.filterwarnings('ignore')
 seed(12345)
 
 
-# TODO: add support for multiple models
-# TODO: add support for multiple optimizers
-
 def draw_samples(y_mean, y_std):
     """Make some predictions by drawing from GP distributions."""
     mean = y_mean.reshape(-1)
@@ -24,18 +21,46 @@ def draw_samples(y_mean, y_std):
     return y_pred
 
 
-def fit_gp(x):
+def log(optimal_hparams, final_error):
+    print(f"ls1: {str(optimal_hparams[0])}, ls2: {str(optimal_hparams[1])}, "
+          f"ls3: {str(optimal_hparams[2])}, v1: {str(optimal_hparams[3])}, "
+          f"v2: {str(optimal_hparams[4])}\n")
+    pass
+
+
+def load_data(lat=51.875, lon=0.9375, total_entries=1980, train_size=0.8):
+    lat_str, lon_str = str(lat).replace(".", ""), str(lon).replace(".", "")
+    pr_df = pd.read_csv(f"{lat_str}_{lon_str}.csv")
+
+    last_train_index = int(total_entries * train_size)
+
+    training_set = pr_df.iloc[0:last_train_index]
+    test_set = pr_df.iloc[last_train_index:total_entries]
+
+    x_train = np.linspace(1, last_train_index, last_train_index, dtype=np.int64)
+    x_test = np.linspace(last_train_index, total_entries, total_entries - last_train_index, dtype=np.int64)
+    x_all = np.concatenate((x_train, x_test))
+
+    # Renormalise precipitation measurements since they're all approximately r*10^-5
+    y_train = (training_set["pr"].to_numpy()) * 10 ** 5
+    y_test = test_set["pr"].to_numpy() * 10 ** 5
+    y_all = np.concatenate([y_train, y_test])
+
+    return x_train, y_train, x_test, y_test, x_all, y_all
+
+
+def fit_gp(hyperparameters):
     """This is the function that GPyOpt will optimise.
-    :parameter x: A vector containing the hyperparameters to optimise.
+    :parameter hyperparameters: A vector containing the hyperparameters to optimise.
     :return: The root mean squared error of the model.
     """
 
-    # TODO: add support for multiple loss functions
+    # TODO: add support for multiple loss functions.
     rmse = 0
-    for i in range(x.shape[0]):
-        # Create and train an SVR on this data.
-        kernel = GPy.kern.RBF(1, lengthscale=x[i, 0], variance=x[i, 3]) + GPy.kern.StdPeriodic(1, lengthscale=x[
-            i, 1]) * GPy.kern.PeriodicMatern32(1, lengthscale=x[i, 2], variance=x[i, 4])
+    for i in range(hyperparameters.shape[0]):
+        kernel = GPy.kern.RBF(1, lengthscale=hyperparameters[i, 0], variance=hyperparameters[i, 3]) + \
+                 GPy.kern.StdPeriodic(1, lengthscale=hyperparameters[i, 1]) * \
+                 GPy.kern.PeriodicMatern32(1, lengthscale=hyperparameters[i, 2], variance=hyperparameters[i, 4])
         model = GPy.models.GPRegression(x_train.reshape(-1, 1), y_train.reshape(-1, 1), kernel=kernel, normalizer=True,
                                         noise_var=0.05)
         y_mean, y_std = model.predict(x_all.reshape(-1, 1))
@@ -49,27 +74,6 @@ def fit_gp(x):
     return rmse
 
 
-def load_data(lat=51.875, lon=0.9375, train_size=0.8):
-    lat_str, lon_str = str(lat), str(lon)
-    pr_df = pd.read_csv(f"{lat_str}_{lon_str}.csv")
-
-    last_train_index = pr_df.size * train_size
-
-    training_set = pr_df.iloc[0:last_train_index]
-    test_set = pr_df.iloc[last_train_index:pr_df.size]
-
-    x_train = np.linspace(1, last_train_index, last_train_index)
-    x_test = np.linspace(last_train_index, pr_df.size, pr_df.size - last_train_index)
-    x_all = np.concatenate((x_train, x_test))
-
-    # Renormalise precipitation measurements since they're all approximately r*10^-5
-    y_train = (training_set["pr"].to_numpy()) * 10 ** 5
-    y_test = test_set["pr"].to_numpy() * 10 ** 5
-    y_all = np.concatenate([y_train, y_test])
-
-    return x_train, y_train, x_test, y_test, x_all, y_all
-
-
 def optimise(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0.1):
     domain = [
         {'name': 'lengthscale1', 'type': 'continuous', 'domain': (0., 5.)},
@@ -77,6 +81,9 @@ def optimise(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0
         {'name': 'lengthscale3', 'type': 'continuous', 'domain': (0., 5.)},
         {'name': 'variance1', 'type': 'continuous', 'domain': (0., 1.)},
         {'name': 'variance2', 'type': 'continuous', 'domain': (0., 1.)}]
+
+    # TODO: add support for multiple models
+    # TODO: add support for multiple optimizers
 
     opt = GPyOpt.methods.BayesianOptimization(f=fit_gp,  # function to optimize
                                               domain=domain,  # box-constraints of the problem
@@ -89,9 +96,8 @@ def optimise(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0
 
     # Get the optimised hyperparameters.
     x_best = opt.X[np.argmin(opt.Y)]
-    print(
-        f"ls1: {str(x_best[0])}, ls2: {str(x_best[1])}, ls3: {str(x_best[2])}, v1: {str(x_best[3])}, v2: {str(x_best[4])}\n")
-    # TODO: get the optimised loss value, log to file.
+    log(x_best, final_error)
+
     # TODO: add logging -  image with composite filename, and log file.
 
     return x_best
@@ -123,6 +129,6 @@ if __name__ == '__main__':
     # TODO: parse arguments
 
     # Load the data.
-    x_train, y_train, x_test, y_test, x_all, y_all = load_data()
-    optimal_hparams = optimise()
+    x_train, y_train, x_test, y_test, x_all, y_all = load_data(total_entries=100)
+    optimal_hparams = optimise(maximum_iterations=30)
     plot_fitted_model(x_train, optimal_hparams, y_train, x_all, x_test, y_test)
