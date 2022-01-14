530ac989c835a2e7071bd9a0d70e4e97086c1e55
Andrei <inwaves@live.com>
2022-01-14 10:43:08 +0000

    WIP script

--- optimise.py
+++ optimise.py
@@ -3,6 +3,7 @@ import GPyOpt
 import numpy as np
 import warnings
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from numpy.random import seed
 from scipy.stats import multivariate_normal as mvn
@@ -10,10 +11,9 @@ from scipy.stats import multivariate_normal as mvn
 warnings.filterwarnings('ignore')
 seed(12345)
 
+
 # TODO: add support for multiple models
-# TODO: add support for multiple datasets
 # TODO: add support for multiple optimizers
-# TODO: add support for multiple loss functions
 
 def draw_samples(y_mean, y_std):
     """Make some predictions by drawing from GP distributions."""
@@ -30,6 +30,7 @@ def fit_gp(x):
     :return: The root mean squared error of the model.
     """
 
+    # TODO: add support for multiple loss functions
     rmse = 0
     for i in range(x.shape[0]):
         # Create and train an SVR on this data.
@@ -43,15 +44,33 @@ def fit_gp(x):
         # Calculate RMSE — does it even make sense to use RMSE since
         # a GP doesn't make exact predictions, but specifies distributions
         # over functions?
-        all_y = np.concatenate([y_train, y_test])
-        rmse += np.sum(all_y - y_pred)
+        rmse += np.sum(y_all - y_pred)
 
     return rmse
 
-def load_data():
-    pass
 
-def train(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0.1):
+def load_data(lat=51.875, lon=0.9375, train_size=0.8):
+    lat_str, lon_str = str(lat), str(lon)
+    pr_df = pd.read_csv(f"{lat_str}_{lon_str}.csv")
+
+    last_train_index = pr_df.size * train_size
+
+    training_set = pr_df.iloc[0:last_train_index]
+    test_set = pr_df.iloc[last_train_index:pr_df.size]
+
+    x_train = np.linspace(1, last_train_index, last_train_index)
+    x_test = np.linspace(last_train_index, pr_df.size, pr_df.size - last_train_index)
+    x_all = np.concatenate((x_train, x_test))
+
+    # Renormalise precipitation measurements since they're all approximately r*10^-5
+    y_train = (training_set["pr"].to_numpy()) * 10 ** 5
+    y_test = test_set["pr"].to_numpy() * 10 ** 5
+    y_all = np.concatenate([y_train, y_test])
+
+    return x_train, y_train, x_test, y_test, x_all, y_all
+
+
+def optimise(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0.1):
     domain = [
         {'name': 'lengthscale1', 'type': 'continuous', 'domain': (0., 5.)},
         {'name': 'lengthscale2', 'type': 'continuous', 'domain': (0., 1.)},
@@ -66,37 +85,44 @@ def train(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0.1)
 
     # Optimise the hyperparameters.
     opt.run_optimization(max_iter=maximum_iterations)
-    opt.plot_convergence() # TODO: get these out to a file.
+    opt.plot_convergence()  # TODO: get these out to a file.
 
     # Get the optimised hyperparameters.
     x_best = opt.X[np.argmin(opt.Y)]
     print(
         f"ls1: {str(x_best[0])}, ls2: {str(x_best[1])}, ls3: {str(x_best[2])}, v1: {str(x_best[3])}, v2: {str(x_best[4])}\n")
-
     # TODO: get the optimised loss value, log to file.
     # TODO: add logging -  image with composite filename, and log file.
 
-def plot_fitted_model(x_train=None, x_best=None, y_train=None, x_all=None, x_test=None, y_test=None):
-    kernel = GPy.kern.RBF(1, lengthscale=x_best[0], variance=x_best[2]) + GPy.kern.PeriodicMatern32(1,
-                                                                                                    lengthscale=x_best[
-                                                                                                        1],
-                                                                                                    variance=x_best[3])
-    model = GPy.models.GPRegression(x_train.reshape(-1, 1), y_train.reshape(-1, 1), kernel=kernel, normalizer=True,
+    return x_best
+
+
+def plot_fitted_model(x_tr, optimal_hparams, y_tr, x_all_p, x_te, y_te):
+    kernel = GPy.kern.RBF(1, lengthscale=optimal_hparams[0], variance=optimal_hparams[3]) + \
+             GPy.kern.StdPeriodic(1, lengthscale=optimal_hparams[1]) * \
+             GPy.kern.PeriodicMatern32(1, lengthscale=optimal_hparams[2], variance=optimal_hparams[4])
+
+    model = GPy.models.GPRegression(x_tr.reshape(-1, 1), y_tr.reshape(-1, 1), kernel=kernel, normalizer=True,
                                     noise_var=0.05)
-    y_mean, y_std = model.predict(x_all.reshape(-1, 1))
+    y_mean, y_std = model.predict(x_all_p.reshape(-1, 1))
 
     # TODO: Get this out to a file.
     plt.figure(figsize=(10, 5), dpi=100)
     plt.xlabel("time")
     plt.ylabel("precipitation")
-    plt.scatter(x_train, y_train, lw=1, color="b", label="training dataset")
-    plt.scatter(x_test, y_test, lw=1, color="r", label="testing dataset")
-    plt.plot(x_all, y_mean, lw=3, color="g", label="GP mean")
-    plt.fill_between(x_all, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
+    plt.scatter(x_tr, y_tr, lw=1, color="b", label="training dataset")
+    plt.scatter(x_te, y_te, lw=1, color="r", label="testing dataset")
+    plt.plot(x_all_p, y_mean, lw=3, color="g", label="GP mean")
+    plt.fill_between(x_all_p, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
                      facecolor="b", alpha=0.3, label="confidence")
     plt.legend(loc="upper left")
     plt.show()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # TODO: parse arguments
 
+    # Load the data.
+    x_train, y_train, x_test, y_test, x_all, y_all = load_data()
+    optimal_hparams = optimise()
+    plot_fitted_model(x_train, optimal_hparams, y_train, x_all, x_test, y_test)
