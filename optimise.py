ba87d2147793a359f93e89189e3adcdef81e5056
Andrei <inwaves@live.com>
2022-01-14 11:52:43 +0000

    Implement actual RMSE, refactor, tidy up, start logging

--- optimise.py
+++ optimise.py
@@ -1,3 +1,5 @@
+import argparse
+
 import GPy
 import GPyOpt
 import numpy as np
@@ -21,11 +23,12 @@ def draw_samples(y_mean, y_std):
     return y_pred
 
 
-def log(optimal_hparams, final_error):
-    print(f"ls1: {str(optimal_hparams[0])}, ls2: {str(optimal_hparams[1])}, "
-          f"ls3: {str(optimal_hparams[2])}, v1: {str(optimal_hparams[3])}, "
-          f"v2: {str(optimal_hparams[4])}\n")
-    pass
+def log(out, experiment_params, optimal_hparams, final_error):
+    out.writelines(f"ls1: {str(optimal_hparams[0])}, ls2: {str(optimal_hparams[1])}, "
+                   f"ls3: {str(optimal_hparams[2])}, v1: {str(optimal_hparams[3])}, "
+                   f"v2: {str(optimal_hparams[4])}, best_error: {final_error}\n")
+
+    out.close()
 
 
 def load_data(lat=51.875, lon=0.9375, total_entries=1980, train_size=0.8):
@@ -51,12 +54,13 @@ def load_data(lat=51.875, lon=0.9375, total_entries=1980, train_size=0.8):
 
 def fit_gp(hyperparameters):
     """This is the function that GPyOpt will optimise.
-    :parameter hyperparameters: A vector containing the hyperparameters to optimise.
-    :return: The root mean squared error of the model.
+        parameter hyperparameters: A vector containing the hyperparameters to optimise.
+        return: The root mean squared error of the model.
     """
 
     # TODO: add support for multiple loss functions.
     rmse = 0
+    print(f"Hyperparams shape: {hyperparameters.shape}")
     for i in range(hyperparameters.shape[0]):
         kernel = GPy.kern.RBF(1, lengthscale=hyperparameters[i, 0], variance=hyperparameters[i, 3]) + \
                  GPy.kern.StdPeriodic(1, lengthscale=hyperparameters[i, 1]) * \
@@ -69,8 +73,12 @@ def fit_gp(hyperparameters):
         # Calculate RMSE — does it even make sense to use RMSE since
         # a GP doesn't make exact predictions, but specifies distributions
         # over functions?
-        rmse += np.sum(y_all - y_pred)
+        rmse += np.sqrt(np.square(y_all - y_pred).mean())
 
+        # This used to be rmse += np.sum(all_y - y_pred)
+        # which isn't the actual RMSE, but gave the positive definite error fewer times.
+
+    print(f"RMSE: {rmse}")
     return rmse
 
 
@@ -96,39 +104,54 @@ def optimise(maximum_iterations=10, acquisition_type="LCB", acquisition_weight=0
 
     # Get the optimised hyperparameters.
     x_best = opt.X[np.argmin(opt.Y)]
-    log(x_best, final_error)
 
-    # TODO: add logging -  image with composite filename, and log file.
-
-    return x_best
-
-
-def plot_fitted_model(x_tr, optimal_hparams, y_tr, x_all_p, x_te, y_te):
     kernel = GPy.kern.RBF(1, lengthscale=optimal_hparams[0], variance=optimal_hparams[3]) + \
              GPy.kern.StdPeriodic(1, lengthscale=optimal_hparams[1]) * \
              GPy.kern.PeriodicMatern32(1, lengthscale=optimal_hparams[2], variance=optimal_hparams[4])
 
-    model = GPy.models.GPRegression(x_tr.reshape(-1, 1), y_tr.reshape(-1, 1), kernel=kernel, normalizer=True,
+    model = GPy.models.GPRegression(x_train.reshape(-1, 1), y_train.reshape(-1, 1), kernel=kernel, normalizer=True,
                                     noise_var=0.05)
-    y_mean, y_std = model.predict(x_all_p.reshape(-1, 1))
 
+    y_mean, y_std = model.predict(x_all.reshape(-1, 1))
+    y_pred = draw_samples(y_mean, y_std)
+
+    # Calculate RMSE — does it even make sense to use RMSE since
+    # a GP doesn't make exact predictions, but specifies distributions
+    # over functions?
+    best_rmse = np.sqrt(np.square(y_all - y_pred).mean())
+
+    return x_best, best_rmse, y_mean, y_std
+
+
+def plot_fitted_model(x_train, x_test, x_all, y_train, y_test, y_mean, y_std, filename):
     # TODO: Get this out to a file.
     plt.figure(figsize=(10, 5), dpi=100)
     plt.xlabel("time")
     plt.ylabel("precipitation")
-    plt.scatter(x_tr, y_tr, lw=1, color="b", label="training dataset")
-    plt.scatter(x_te, y_te, lw=1, color="r", label="testing dataset")
-    plt.plot(x_all_p, y_mean, lw=3, color="g", label="GP mean")
-    plt.fill_between(x_all_p, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
+    plt.scatter(x_train, y_train, lw=1, color="b", label="training dataset")
+    plt.scatter(x_test, y_test, lw=1, color="r", label="testing dataset")
+    plt.plot(x_all, y_mean, lw=3, color="g", label="GP mean")
+    plt.fill_between(x_all, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
                      facecolor="b", alpha=0.3, label="confidence")
     plt.legend(loc="upper left")
-    plt.show()
+    plt.savefig(filename)
 
 
 if __name__ == '__main__':
-    # TODO: parse arguments
+    parser = argparse.ArgumentParser(description='Run the GPyOpt optimisation algorithm.')
 
+    # lat, lon, how many entries
+    # how many iterations, domain1, domain2, domain3, domain4, domain5
+
+    # TODO: parse the args, collect them into a list for logging
+
+    experiment_params = []
     # Load the data.
     x_train, y_train, x_test, y_test, x_all, y_all = load_data(total_entries=100)
-    optimal_hparams = optimise(maximum_iterations=30)
-    plot_fitted_model(x_train, optimal_hparams, y_train, x_all, x_test, y_test)
+    optimal_hparams, best_error, y_mean, y_std = optimise(maximum_iterations=10)
+    composite_filename = f"something"
+
+    out = open(f"{composite_filename}.txt", "w")
+    log(out, experiment_params, optimal_hparams, best_error)
+
+    plot_fitted_model(x_train, x_test, x_all, y_train, y_test, y_mean, y_std, f"{composite_filename}.png")
