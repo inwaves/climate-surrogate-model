5fb54e69555a48b098ac53caea177b8ba19b1d13
Andrei <inwaves@live.com>
2022-01-17 12:38:01 +0000

    Refactor to prepare for geospatial data

--- optimise.py
+++ optimise.py
@@ -4,53 +4,15 @@ import itertools
 
 import GPy
 import GPyOpt
-import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import pandas as pd
 
 from numpy.random import seed
-from utils.utils import draw_samples, log, rms_error, parse_args
+from utils.utils import draw_samples, log, rms_error, parse_args, load_data_single_coordinate, plot_model_predictions
 
 warnings.filterwarnings('ignore')
 seed(12345)
 sns.set()
-
-
-# TODO: load global dataset instead of one single point. x[t] = [lat, lon, t]
-def load_data(lat: float = 51.875, lon: float = 0.9375, total_entries: int = 1980, train_size: float = 0.8) -> tuple:
-    """Load data from a .csv file with the specified parameters.
-        :param lat: The latitude of the location.
-        :param lon: The longitude of the location.
-        :param total_entries: The total number of months to consider starting in Jan 1850.
-        :param train_size: The proportion of the data to use for training.
-        :return: A tuple containing the training set, the test set, as well as the two sets combined.
-    """
-    lat_str, lon_str = str(lat).replace(".", "_"), str(lon).replace(".", "_")
-    pr_df = pd.read_csv(f"./data/{lat_str}x{lon_str}.csv")
-
-    last_train_index = int(total_entries * train_size)
-    last_validation_index = int(last_train_index + ((total_entries - last_train_index) // 3))
-
-    training_set = pr_df.iloc[0:last_train_index]
-    validation_set = pr_df.iloc[last_train_index:last_validation_index]
-    test_set = pr_df.iloc[last_validation_index:total_entries]
-
-    x_train = np.linspace(1, last_train_index,
-                          last_train_index, dtype=np.int64)
-    x_valid = np.linspace(last_train_index, last_validation_index,
-                          last_validation_index - last_train_index, dtype=np.int64)
-    x_test = np.linspace(last_validation_index, total_entries,
-                         total_entries - last_validation_index, dtype=np.int64)
-    x_all = np.concatenate((x_train, x_valid, x_test))
-
-    # Renormalise precipitation measurements since they're all approximately r*10^-5
-    y_train = (training_set["pr"].to_numpy()) * 10 ** 5
-    y_valid = (validation_set["pr"].to_numpy()) * 10 ** 5
-    y_test = test_set["pr"].to_numpy() * 10 ** 5
-    y_all = np.concatenate([y_train, y_valid, y_test])
-
-    return x_train, y_train, x_valid, y_valid, x_test, y_test, x_all, y_all
 
 
 def fit_gp_k1(hyperparameters: np.ndarray) -> float:
@@ -144,7 +106,8 @@ def grid_search_domains(k: int, kernel: int, maximum_iterations: int, model_type
 
         # Run optimisation using the domains generated.
         tic = time.perf_counter()
-        optimal_hparams, best_error, y_mean, y_std = optimise(maximum_iterations=maximum_iterations, kernel_choice=args.kernel,
+        optimal_hparams, best_error, y_mean, y_std = optimise(maximum_iterations=maximum_iterations,
+                                                              kernel_choice=args.kernel,
                                                               dom_tuples=dom_tuples, model_type=model_type,
                                                               initial_design_type=initial_design_type,
                                                               acquisition_type=acquisition_type,
@@ -153,15 +116,15 @@ def grid_search_domains(k: int, kernel: int, maximum_iterations: int, model_type
         toc = time.perf_counter()
 
         log(out, kernel, args, optimal_hparams, best_error, dom_tuples, toc - tic)
-        plot_fitted_model(x_train, x_valid, x_test, x_all,
-                          y_train, y_valid, y_test, y_mean, y_std,
+        plot_model_predictions(x_train, x_valid, x_test, x_all,
+                               y_train, y_valid, y_test, y_mean, y_std,
                           f"{composite_filename}.png")
     out.close()
 
 
-def optimise(maximum_iterations: int = 10, kernel_choice: int = 1, dom_tuples: list[tuple] = None, model_type: str = "GP",
-             initial_design_type: str = "random", acquisition_type: str = "LCB", acquisition_weight: float = 0.1,
-             acquisition_optimiser_type: str = "lbfgs") -> tuple:
+def optimise(maximum_iterations: int = 10, kernel_choice: int = 1, dom_tuples: list[tuple] = None,
+             model_type: str = "GP", initial_design_type: str = "random", acquisition_type: str = "LCB",
+             acquisition_weight: float = 0.1, acquisition_optimiser_type: str = "lbfgs") -> tuple:
     """Runs the optimisation algorithm for a given set of hyperparameter domains.
         Also takes in other hyperparameters for the optimisation procedure.
         :param kernel_choice: Kernel 1 is RBF-Cosine, kernel 2 is RBF-Cosine-Noise.
@@ -237,7 +200,7 @@ def optimise(maximum_iterations: int = 10, kernel_choice: int = 1, dom_tuples: l
                  GPy.kern.sde_RatQuad(1, lengthscale=optimal_hparams[3], variance=optimal_hparams[8]) + \
                  (GPy.kern.RBF(1, lengthscale=optimal_hparams[4], variance=optimal_hparams[9]) *
                   GPy.kern.sde_White(1, variance=optimal_hparams[10]))
-        
+
     model = GPy.models.GPRegression(x_train.reshape(-1, 1), (y_train - y_mean).reshape(-1, 1), kernel=kernel,
                                     normalizer=False, noise_var=0.05)
 
@@ -251,41 +214,14 @@ def optimise(maximum_iterations: int = 10, kernel_choice: int = 1, dom_tuples: l
     return optimal_hparams, best_rmse, y_mean, y_std
 
 
-def plot_fitted_model(x_train: np.ndarray, x_valid: np.ndarray, x_test: np.ndarray, x_all: np.ndarray,
-                      y_train: np.ndarray, y_valid: np.ndarray, y_test: np.ndarray, y_mean: np.ndarray,
-                      y_std: np.ndarray, filename) -> None:
-    """Plots a fitted GP model's predictions.
-        :param x_train: The training set months.
-        :param x_valid: The validation set months.
-        :param x_test: The test set months.
-        :param x_all: The full training + test set.
-        :param y_train: The training precipitation values.
-        :param y_test: The test precipitation values.
-        :param y_valid: The validation precipitation values.
-        :param y_mean: The mean of the predictions.
-        :param y_std: The standard deviation of the predictions.
-        :param filename: The filename to save the plot to.
-    """
-    plt.figure(figsize=(10, 5), dpi=100)
-    plt.xlabel("time")
-    plt.ylabel("residual")
-    plt.scatter(x_train, y_train - y_train.mean(), lw=1, color="b", label="training dataset")
-    plt.scatter(x_valid, y_valid - y_valid.mean(), lw=1, color="y", label="validation dataset")
-    plt.scatter(x_test, y_test - y_test.mean(), lw=1, color="r", label="testing dataset")
-    plt.plot(x_all, y_mean, lw=3, color="g", label="GP mean")
-    plt.fill_between(x_all, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
-                     facecolor="b", alpha=0.3, label="confidence")
-    plt.legend(loc="upper left")
-    plt.savefig(filename)
-
-
 if __name__ == '__main__':
     args = parse_args()
 
     # Load the data.
-    x_train, y_train, x_test, y_test, x_valid, y_valid, x_all, y_all = load_data(lat=args.lat,
-                                                                                 lon=args.lon,
-                                                                                 total_entries=args.num_months)
+    data = load_data_single_coordinate(lat=args.lat,
+                                       lon=args.lon,
+                                       num_months=args.num_months)
+    x_train, y_train, x_test, y_test, x_valid, y_valid, x_all, y_all = data
 
     composite_filename = f"./logs/{args.model_type}_{args.acquisition_type}_{args.acquisition_optimiser_type}_" \
                          f"{str(args.lat).replace('.', 'p')}_{str(args.lon).replace('.', 'p')}_" \
@@ -296,7 +232,7 @@ if __name__ == '__main__':
     # with default domains.
     if args.grid_search:
         num_hparams = 8 if args.kernel == 1 else 11  # The first kernel has 8 hparams, the second 11.
-        grid_search_domains(k=args.k, num_hyperparameters=num_hparams,
+        grid_search_domains(k=args.k, kernel=args.kernel,
                             maximum_iterations=args.num_iterations, model_type=args.model_type,
                             initial_design_type=args.initial_design, acquisition_type=args.acquisition_type,
                             acquisition_weight=args.acquisition_weight,
@@ -307,8 +243,8 @@ if __name__ == '__main__':
             dom_tuples = [(0., 30.), (0., 30.), (0., 30.), (0., 30.),  # Length scale domains.
                           (0., 10.), (0., 10.), (0., 10.), (0., 10.)]  # Variance domains.
         else:
-            dom_tuples = [(0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.),             # Length scale domains.
-                          (0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.)]   # Variance domains.
+            dom_tuples = [(0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.),  # Length scale domains.
+                          (0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.), (0., 10.)]  # Variance domains.
 
         tic = time.perf_counter()
         optimal_hparams, best_error, y_mean, y_std = optimise(maximum_iterations=args.num_iterations,
@@ -317,12 +253,13 @@ if __name__ == '__main__':
                                                               initial_design_type=args.initial_design,
                                                               acquisition_type=args.acquisition_type,
                                                               acquisition_weight=args.acquisition_weight,
-                                                              acquisition_optimiser_type=args.acquisition_optimiser_type)
+                                                              acquisition_optimiser_type=args.acquisition_optimiser_type
+                                                              )
         toc = time.perf_counter()
 
         out = open(f"{composite_filename}.txt", "a+")
         log(out, args.kernel, args, optimal_hparams, best_error, dom_tuples, toc - tic)
         out.close()
-        plot_fitted_model(x_train, x_valid, x_test, x_all,
-                          y_train, y_valid, y_test, y_mean, y_std,
+        plot_model_predictions(x_train, x_valid, x_test, x_all,
+                               y_train, y_valid, y_test, y_mean, y_std,
                           f"{composite_filename}.png")
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import argparse
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 from scipy.stats import multivariate_normal as mvn
 
 
@@ -79,3 +81,72 @@ def parse_args():
                         help="Percentage of the data to use for training. Defaults to 0.8.")
 
     return parser.parse_args()
+
+
+def load_data(num_months: int = 1980, training_proportion: float = 0.8) -> tuple:
+    pass
+
+
+def load_data_single_coordinate(lat: float = 51.875, lon: float = 0.9375, num_months: int = 1980,
+                                training_proportion: float = 0.8) -> tuple:
+    """Load data from a .csv file with the specified parameters.
+        :param lat: The latitude of the location.
+        :param lon: The longitude of the location.
+        :param num_months: The total number of months to consider starting in Jan 1850.
+        :param training_proportion: The proportion of the data to use for training.
+        :return: A tuple containing the training set, the test set, as well as the two sets combined.
+    """
+    # TODO: load global dataset instead of one single point. x[t] = [lat, lon, t]
+    lat_str, lon_str = str(lat).replace(".", "_"), str(lon).replace(".", "_")
+    pr_df = pd.read_csv(f"./data/{lat_str}x{lon_str}.csv")
+
+    last_train_index = int(num_months * training_proportion)
+    last_validation_index = int(last_train_index + ((num_months - last_train_index) // 3))
+
+    training_set = pr_df.iloc[0:last_train_index]
+    validation_set = pr_df.iloc[last_train_index:last_validation_index]
+    test_set = pr_df.iloc[last_validation_index:num_months]
+
+    x_train = np.linspace(1, last_train_index,
+                          last_train_index, dtype=np.int64)
+    x_valid = np.linspace(last_train_index, last_validation_index,
+                          last_validation_index - last_train_index, dtype=np.int64)
+    x_test = np.linspace(last_validation_index, num_months,
+                         num_months - last_validation_index, dtype=np.int64)
+    x_all = np.concatenate((x_train, x_valid, x_test))
+
+    # Renormalise precipitation measurements since they're all approximately r*10^-5
+    y_train = (training_set["pr"].to_numpy()) * 10 ** 5
+    y_valid = (validation_set["pr"].to_numpy()) * 10 ** 5
+    y_test = test_set["pr"].to_numpy() * 10 ** 5
+    y_all = np.concatenate([y_train, y_valid, y_test])
+
+    return x_train, y_train, x_valid, y_valid, x_test, y_test, x_all, y_all
+
+
+def plot_model_predictions(x_train: np.ndarray, x_valid: np.ndarray, x_test: np.ndarray, x_all: np.ndarray,
+                           y_train: np.ndarray, y_valid: np.ndarray, y_test: np.ndarray, y_mean: np.ndarray,
+                           y_std: np.ndarray, filename) -> None:
+    """Plots a fitted GP model's predictions.
+        :param x_train: The training set months.
+        :param x_valid: The validation set months.
+        :param x_test: The test set months.
+        :param x_all: The full training + test set.
+        :param y_train: The training precipitation values.
+        :param y_test: The test precipitation values.
+        :param y_valid: The validation precipitation values.
+        :param y_mean: The mean of the predictions.
+        :param y_std: The standard deviation of the predictions.
+        :param filename: The filename to save the plot to.
+    """
+    plt.figure(figsize=(10, 5), dpi=100)
+    plt.xlabel("time")
+    plt.ylabel("residual")
+    plt.scatter(x_train, y_train - y_train.mean(), lw=1, color="b", label="training dataset")
+    plt.scatter(x_valid, y_valid - y_valid.mean(), lw=1, color="y", label="validation dataset")
+    plt.scatter(x_test, y_test - y_test.mean(), lw=1, color="r", label="testing dataset")
+    plt.plot(x_all, y_mean, lw=3, color="g", label="GP mean")
+    plt.fill_between(x_all, (y_mean + y_std).reshape(y_mean.shape[0]), (y_mean - y_std).reshape(y_mean.shape[0]),
+                     facecolor="b", alpha=0.3, label="confidence")
+    plt.legend(loc="upper left")
+    plt.savefig(filename)
